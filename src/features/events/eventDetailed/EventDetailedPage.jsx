93c1a09ece8de91fc3cd0be10705b280f7887336
Orkhan Mammadli <orkhanmm@code.edu.az>
2021-01-20 05:54:05 +0400

    added ErrorComponent.js

--- src/features/events/eventDetailed/EventDetailedPage.jsx
+++ src/features/events/eventDetailed/EventDetailedPage.jsx
@@ -4,19 +4,37 @@ import EventDetailedHeader from "./EventDetailedHeader";
 import EventDetailedInfo from "./EventDetailedInfo";
 import EventDetailedChat from "./EventDetailedChat";
 import EventDetailedSideBar from "./EventDetailedSideBar";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import useFirestoreDoc from "../../../app/hooks/useFirestoreDoc";
+import {listenToEvents} from "../eventActions";
+import {listenToEventFromFirestore} from "../../../app/firestore/firestoreService";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import {Redirect} from 'react-router-dom';
 
 const EventDetailedPage = ({match}) => {
-    const event=useSelector(state=>state.event.events.find(e=>e.id===match.params.id));
+    const dispatch=useDispatch();
+    const event = useSelector(state => state.event.events.find(e => e.id === match.params.id));
+    const {loading,error} = useSelector((state) => state.async)
+
+    useFirestoreDoc({
+        query: () => listenToEventFromFirestore(match.params.id),
+        data: event => dispatch(listenToEvents([event])),
+        deps: [match.params.id,dispatch]
+    })
+
+    if(loading || (!event && !error)) return <LoadingComponent content='Loading event...' />
+
+    if(error) return <Redirect to='/error' />
+
     return (
         <Grid>
             <GridColumn width={10}>
-                <EventDetailedHeader event={event} />
-                <EventDetailedInfo event={event} />
-                <EventDetailedChat />
+                <EventDetailedHeader event={event}/>
+                <EventDetailedInfo event={event}/>
+                <EventDetailedChat/>
             </GridColumn>
             <GridColumn width={6}>
-                <EventDetailedSideBar attendees={event.attendees} />
+                <EventDetailedSideBar attendees={event?.attendees}/>
             </GridColumn>
         </Grid>
     );
